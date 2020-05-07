fe56ac0fb917125b5e1aff95b2854514e77b365b
atoumbre <atoumbre@gmail.com>
2020-05-07 11:28:47 +0000

    Update colors.dart
    Creates conflict with Colors class from flutter Material library.

--- lib/src/model/colors.dart
+++ lib/src/model/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-class Colors {
+class EqColors {
   static const Color black = Color(0xFF000000);
   static const Color transparent = Color(0x00000000);
   static const Color white = Color(0xFFFFFFFF);
